076228a1eef41a3c18d7c5a66c95d1cc8b5d6d0f
Tomasz Płocic <tplocic20@gmail.com>
2023-12-18 11:41:35 +0100

    proposal form UX tweaks

--- app/interfaces/create-proposal.interface.ts
+++ app/interfaces/create-proposal.interface.ts
@@ -1,7 +1,28 @@
 export interface CreateProposalForm {
-    wallet: CreateProposalFormStep;
+    [CreateProposalFormStepEnum.wallet]: CreateProposalFormStep;
+    [CreateProposalFormStepEnum.content]: CreateProposalFormStep;
 }
 
 interface CreateProposalFormStep {
     isValid: boolean;
+    isEnable: boolean;
+    value: { [key: string]: string }
+}
+
+export enum CreateProposalFormStepEnum {
+    wallet = 'wallet',
+    content = 'content'
+}
+
+export const initialCreateProposalForm: CreateProposalForm = {
+    [CreateProposalFormStepEnum.wallet]: {
+        isValid: false,
+        isEnable: true,
+        value: {}
+    },
+    [CreateProposalFormStepEnum.content]: {
+        isValid: false,
+        isEnable: false,
+        value: {}
+    }
 }
--- app/providers/create-proposal.provider.tsx
+++ app/providers/create-proposal.provider.tsx
@@ -1,10 +1,50 @@
-import {createContext, ReactNode} from "react";
-import {CreateProposalForm} from "@interfaces/create-proposal.interface";
+import {createContext, ReactNode, useContext, useState} from "react";
+import {
+    CreateProposalForm,
+    CreateProposalFormStepEnum,
+    initialCreateProposalForm
+} from "@interfaces/create-proposal.interface";
 
-
-export const CreateProposalContext = createContext({
-
+const CreateProposalContext = createContext({
+    form: initialCreateProposalForm as CreateProposalForm,
+    setIsValid: (step: CreateProposalFormStepEnum, value: boolean) => {
+    },
+    setIsEnabled: (step: CreateProposalFormStepEnum, value: boolean) => {
+    }
 });
 export const CreateProposalProvider = ({children}: { children: ReactNode }) => {
 
+    const [form, setForm] = useState(initialCreateProposalForm);
+
+    const setIsValid = (step: CreateProposalFormStepEnum, value: boolean) => {
+        const stepValue = form[step];
+        setForm({
+            ...form, [step]: {
+                ...stepValue,
+                isValid: value
+            }
+        });
+    }
+
+    const setIsEnabled = (step: CreateProposalFormStepEnum, value: boolean) => {
+        const stepValue = form[step];
+        setForm({
+            ...form, [step]: {
+                ...stepValue,
+                isEnabled: value
+            }
+        });
+    }
+
+    return <CreateProposalContext.Provider value={{
+        form,
+        setIsValid,
+        setIsEnabled
+    }}>
+        {children}
+    </CreateProposalContext.Provider>
+}
+
+export const useCreateProposalContext = () => {
+    return useContext(CreateProposalContext);
 }

ff6911d82d23a7e62aa6175abde8729deff0d219
Tomasz Płocic <tplocic20@gmail.com>
2023-12-28 14:41:47 +0100

    extend the generic modal to allow cascading modals

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -6,8 +6,9 @@ import {Inter} from 'next/font/google';
 import {Footer, Header} from "@components/_shared";
 import React, {ReactNode} from "react";
 import {Providers} from "@/app/(routes)/providers";
-import { Breadcrumbs } from '../components/_shared/breadcrumbs/breadcrumbs.component';
-import { usePathname } from 'next/navigation';
+import {ModalProvider} from "@/app/providers/modal.provider";
+import {Breadcrumbs} from '../components/_shared/breadcrumbs/breadcrumbs.component';
+import {usePathname} from 'next/navigation';
 
 const inter = Inter({subsets: ['latin']})
 
@@ -20,19 +21,21 @@ export default function RootLayout({children}: RootLayoutProps) {
     const homePage = path === '/';
 
     return (
-        <html lang="en">
-        <body className={inter.className}>
-        <Providers>
-            <div className="min-h-screen flex flex-col">
-                <Header/>
-                <div className="main-container px-6 min-[1160px]:px-0 mt-32 md:mt-0 flex-1">
-                {!homePage && <Breadcrumbs/>}
-                    {children}
-                </div>
-                <Footer className="pt-6"/>
-            </div>
-        </Providers>
-        </body>
-        </html>
+            <html lang="en">
+            <body className={inter.className}>
+            <Providers>
+                <ModalProvider>
+                    <div className="min-h-screen flex flex-col">
+                        <Header/>
+                        <div className="main-container px-6 min-[1160px]:px-0 mt-32 md:mt-0 flex-1">
+                            {!homePage && <Breadcrumbs/>}
+                            {children}
+                        </div>
+                        <Footer className="pt-6"/>
+                    </div>
+                </ModalProvider>
+            </Providers>
+            </body>
+            </html>
     )
 }

598d5a1b08f0af17819cf92310422a1836145107
bowd <dumbogdan@gmail.com>
2024-01-12 17:50:01 +0100

    feat: update to next 14

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // See: https://github.com/WalletConnect/walletconnect-monorepo/issues/1908#issuecomment-1712429322
+  webpack: (config, context) => {
+    if (config.plugins) {
+      config.plugins.push(
+        new context.webpack.IgnorePlugin({
+          resourceRegExp: /^(lokijs|pino-pretty|encoding)$/,
+        })
+      );
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

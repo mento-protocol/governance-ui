f0e83f0424fb96d1fcdbddfc5be69cb808145b1d
Anna <anna.jezierska@neti-soft.com>
2024-01-15 23:46:01 +0100

    Add changes to 'Cast votes' container

--- app/(routes)/proposals/[id]/page.tsx
+++ app/(routes)/proposals/[id]/page.tsx
@@ -1,33 +1,24 @@
 "use client";
-import { stateToBadgeColorMap } from "@interfaces/proposal.interface";
+import { MarkdownView } from "@/app/components/_shared/markdown-view/markdown-view.component";
+import useModal from "@/app/providers/modal.provider";
+import { useProposalDetailsStore, useUserStore } from "@/app/store";
 import {
   Avatar,
   Badge,
   Button,
   Card,
-  Input,
   Loader,
   TabList,
-  WalletAddressWithCopy,
+  WalletAddressWithCopy
 } from "@components/_shared";
 import { Countdown } from "@components/countdown/countdown.component";
-import { format } from "date-fns";
-import { useForm } from "react-hook-form";
-import styles from "./page.module.scss";
-import { InferType, number, object, setLocale } from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { VotesList } from "@components/votes-list/votes-list.component";
-import classNames from "classnames";
-import { useEffect, useState } from "react";
-import { MarkdownView } from "@/app/components/_shared/markdown-view/markdown-view.component";
-import useModal from "@/app/providers/modal.provider";
+import { stateToBadgeColorMap } from "@interfaces/proposal.interface";
 import { IVoteType } from "@interfaces/vote.interface";
-import { useProposalDetailsStore, useUserStore } from "@/app/store";
-
-const validationSchema = object({
-  votingPower: number().required().typeError("Invalid number").max(400),
-});
-type FormData = InferType<typeof validationSchema>;
+import classNames from "classnames";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import styles from "./page.module.scss";
 
 const voteTypeToModalType = (voteType: IVoteType) => {
   switch (voteType) {
@@ -42,7 +33,7 @@ const voteTypeToModalType = (voteType: IVoteType) => {
 };
 
 const Page = ({ params }: { params: { id: string } }) => {
-  const { showConfirm, showModal } = useModal();
+  const { showConfirm } = useModal();
 
   const { proposal, isFetching, fetch, vote } = useProposalDetailsStore();
   const { walletAddress, balanceVeMENTO } = useUserStore();
@@ -51,38 +42,18 @@ const Page = ({ params }: { params: { id: string } }) => {
     fetch(params.id);
   }, [params.id, fetch]);
 
-  setLocale({
-    mixed: {
-      default: "Invalid number",
-    },
-    number: {
-      max: ({ max }) => `Must not exceed ${max}`,
-    },
-  });
-
   const [votingOpened, setVotingOpened] = useState(false);
   const [votesListOpened, setVotesListOpened] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors, isValid },
-  } = useForm<FormData>({
-    resolver: yupResolver(validationSchema),
-    mode: "all",
-  });
-
-  const onSubmit = (data: FormData, voteType: IVoteType) => {
+  const onSubmit = (voteType: IVoteType) => {
     showConfirm(
-      `Are you sure you want to vote with ${data.votingPower} power?`,
+      `Are you sure you want to vote with ${balanceVeMENTO} power?`,
       {
         modalType: voteTypeToModalType(voteType),
       },
     ).then((result) => {
       if (result) {
-        setValue("votingPower", 0);
-        vote(voteType, data.votingPower, "xD");
+        vote(voteType, balanceVeMENTO, walletAddress || '');
       }
     });
   };
@@ -169,8 +140,8 @@ const Page = ({ params }: { params: { id: string } }) => {
                     !walletAddress && "!opacity-60",
                   )}
                 >
-                  <Card.Header className="text-center text-2xl">
-                    <strong>Voting</strong>
+                  <Card.Header className="text-center">
+                    <h2 className={styles.votes_title}>Cast votes</h2>
                     <button
                       className={styles.proposal_addon__close}
                       onClick={() => setVotingOpened(false)}
@@ -179,63 +150,46 @@ const Page = ({ params }: { params: { id: string } }) => {
                     </button>
                   </Card.Header>
                   <div className="flex flex-col gap-1 ">
-                    <Input
-                      label="Voting power"
-                      id="voting-power"
-                      type="number"
-                      className={styles.input}
-                      placeholder="Voting power"
-                      disabled={!walletAddress}
-                      form={{ ...register("votingPower") }}
-                      error={errors.votingPower?.message}
-                      addon={
-                        <div className={styles.addon}>
-                          <div className="flex justify-between">
-                            {!!walletAddress ? (
-                              <>
-                                <div className="underline">Max available</div>
-                                <div>{balanceVeMENTO} MENT</div>
-                              </>
-                            ) : (
-                              <div className="underline">
-                                Please connect wallet
-                              </div>
-                            )}
-                          </div>
+                  <div className={styles.addon}>
+                    <div className="flex justify-center">
+                      {!!walletAddress ? (
+                        <div className={styles.power}>
+                          <div className={styles.power__title}>Your voting power</div>
+                          <div className={styles.power__value}>{balanceVeMENTO} veMENTO</div>
                         </div>
-                      }
-                    />
-                    <p className={styles.vote_label}>Vote</p>
+                      ) : (
+                        <div className="underline">
+                          Please connect wallet
+                        </div>
+                      )}
+                    </div>
+                  </div>
                     <Button
                       className={styles.button_wrapper}
-                      disabled={!isValid}
+                      disabled={!walletAddress}
                       theme="success"
                       block
-                      onClick={handleSubmit((data) => onSubmit(data, "for"))}
+                      onClick={() => onSubmit("for")}
                     >
                       For
                     </Button>
                     <Button
                       className={styles.button_wrapper}
-                      disabled={!isValid}
+                      disabled={!walletAddress}
                       type="submit"
                       theme="danger"
                       block
-                      onClick={handleSubmit((data) =>
-                        onSubmit(data, "against"),
-                      )}
+                      onClick={() => onSubmit("against")}
                     >
                       Against
                     </Button>
                     <Button
                       className={styles.button_wrapper}
-                      disabled={!isValid}
+                      disabled={!walletAddress}
                       type="submit"
                       theme="tertiary"
                       block
-                      onClick={handleSubmit((data) =>
-                        onSubmit(data, "abstain"),
-                      )}
+                      onClick={() => onSubmit("abstain")}
                     >
                       Abstain
                     </Button>

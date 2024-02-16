506d10ea1ec942712dd6f4db8af1a8dd2518a23f
Philip Paetz <philip.paetz@me.com>
2024-02-16 10:55:47 +0100

    feat(22): linked blockie and shortened address to block explorer

--- app/components/wallet-address/wallet-address.component.tsx
+++ app/components/wallet-address/wallet-address.component.tsx
@@ -1,5 +1,6 @@
-import BaseComponentProps from "@interfaces/base-component-props.interface";
 import WalletHelper from "@/app/helpers/wallet.helper";
+import BaseComponentProps from "@interfaces/base-component-props.interface";
+import { useAccount } from "wagmi";
 
 interface WalletAddressProps extends BaseComponentProps {
   address: string;
@@ -10,9 +11,22 @@ export const WalletAddress = ({
   className,
   style,
 }: WalletAddressProps) => {
+  const { chain } = useAccount();
+  const blockExplorerUrl = chain?.blockExplorers?.default.url;
+  const shortAddress = WalletHelper.getShortAddress(address);
   return (
     <div className={className} style={style}>
-      {WalletHelper.getShortAddress(address)}
+      {blockExplorerUrl ? (
+        <a
+          href={`${blockExplorerUrl}/address/${address}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {shortAddress}
+        </a>
+      ) : (
+        shortAddress
+      )}
     </div>
   );
 };

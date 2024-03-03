f056576fcd3d560a31e2128ae6e5e37ae052c262
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-03-03 02:18:54 -0500

    feat: add mento loading icon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: { "primary-blue": "#4D62F0" },
+    keyframes: {
+      "spin-slow": {
+        "0%": { transform: "rotate(0deg)" },
+        "100%": { transform: "rotate(360deg)" },
+      },
+    },
+    animation: {
+      "spin-slow": "spin-slow 1.5s linear infinite",
+    },
   },
-  plugins: [
-      require('@tailwindcss/typography')
-  ],
-}
-export default config
+  plugins: [require("@tailwindcss/typography")],
+};
+export default config;

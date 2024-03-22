0397512e4c5317c1b84a92768a9e229f3e4195f2
Ryan Noble <ryanjnoble@gmail.com>
2024-03-22 12:00:57 +0100

    Use proposals hook

--- src/components/proposals-list/proposals-list.component.tsx
+++ src/components/proposals-list/proposals-list.component.tsx
@@ -1,8 +1,5 @@
-import { GetProposals, Proposal } from "@/lib/graphql";
 import NumbersService from "@/lib/helpers/numbers.service";
 import StringService from "@/lib/helpers/string.service";
-import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
-import { useChainId } from "wagmi";
 import styles from "./proposals-list.module.scss";
 import { useProposalStates } from "@/lib/contracts/governor/useProposalStates";
 import BaseComponentProps from "@/lib/interfaces/base-component-props.interface";
@@ -11,6 +8,7 @@ import { Card, ProgressBar, Status } from "@/components/_shared";
 import Link from "next/link";
 import { stateToStatusColorMap } from "@/lib/interfaces/proposal.interface";
 import { formatUnits } from "viem";
+import useGetProposals from "@/lib/contracts/governor/useGetProposals";
 
 interface ProposalsListProps extends BaseComponentProps {}
 
@@ -18,15 +16,9 @@ export const ProposalsListComponent = ({
   className,
   style,
 }: ProposalsListProps) => {
-  const chainId = useChainId();
-  const { data } = useSuspenseQuery<{ proposals: Proposal[] }>(GetProposals, {
-    queryKey: "GetProposals",
-    context: {
-      apiName: chainId === 44787 ? "subgraphAlfajores" : "subgraph",
-    },
-  });
+  const { proposals } = useGetProposals();
 
-  useProposalStates(data?.proposals);
+  useProposalStates(proposals);
 
   return (
     <div
@@ -49,75 +41,64 @@ export const ProposalsListComponent = ({
             <div className={styles.header}>Votes against</div>
             <div className={styles.header}>Total votes</div>
           </div>
-          {data?.proposals.map(
-            ({ proposalId, metadata, state, votes }, index) => (
+          {proposals.map(({ proposalId, metadata, state, votes }, index) => (
+            <div key={index} className={classNames(styles.proposals_grid__row)}>
+              {!!index && <div className={styles.divider} />}
               <div
-                key={index}
-                className={classNames(styles.proposals_grid__row)}
+                className={classNames(
+                  styles.proposals_grid__row__element,
+                  styles.first,
+                )}
               >
-                {!!index && <div className={styles.divider} />}
-                <div
-                  className={classNames(
-                    styles.proposals_grid__row__element,
-                    styles.first,
-                  )}
-                >
-                  <div className="flex gap-x3 place-items-center">
-                    <Link
-                      className="flex-1"
-                      style={{ maxHeight: "3em" }}
-                      href={`/proposals/${proposalId}`}
-                    >
-                      <p>
-                        {StringService.limitLength(
-                          `${metadata?.title}`,
-                          75,
-                          true,
-                        )}
-                      </p>
-                    </Link>
-                  </div>
-                </div>
-                <Status
-                  text={state?.toString()}
-                  type={stateToStatusColorMap[state]}
-                />
-                <div
-                  className={classNames(styles.proposals_grid__row__element)}
-                >
-                  <ProgressBar
-                    type="success"
-                    className={styles.progress_bar}
-                    current={Number(formatUnits(votes.for.total, 18))}
-                    max={Number(formatUnits(votes.total, 18))}
-                    valueFormat="alphabetic"
-                  />
-                </div>
-                <div
-                  className={classNames(styles.proposals_grid__row__element)}
-                >
-                  <ProgressBar
-                    type="danger"
-                    className={styles.progress_bar}
-                    current={Number(formatUnits(votes.against.total, 18))}
-                    max={Number(formatUnits(votes.total, 18))}
-                    valueFormat="alphabetic"
-                  />
-                </div>
-                <div
-                  className={classNames(
-                    styles.proposals_grid__row__element,
-                    styles.last,
-                    "mb-3",
-                  )}
-                >
-                  {NumbersService.parseNumericValue(
-                    formatUnits(votes.total, 18),
-                  )}
+                <div className="flex gap-x3 place-items-center">
+                  <Link
+                    className="flex-1"
+                    style={{ maxHeight: "3em" }}
+                    href={`/proposals/${proposalId}`}
+                  >
+                    <p>
+                      {StringService.limitLength(
+                        `${metadata?.title}`,
+                        75,
+                        true,
+                      )}
+                    </p>
+                  </Link>
                 </div>
               </div>
-            ),
-          )}
+              <Status
+                text={state?.toString()}
+                type={stateToStatusColorMap[state]}
+              />
+              <div className={classNames(styles.proposals_grid__row__element)}>
+                <ProgressBar
+                  type="success"
+                  className={styles.progress_bar}
+                  current={Number(formatUnits(votes.for.total, 18))}
+                  max={Number(formatUnits(votes.total, 18))}
+                  valueFormat="alphabetic"
+                />
+              </div>
+              <div className={classNames(styles.proposals_grid__row__element)}>
+                <ProgressBar
+                  type="danger"
+                  className={styles.progress_bar}
+                  current={Number(formatUnits(votes.against.total, 18))}
+                  max={Number(formatUnits(votes.total, 18))}
+                  valueFormat="alphabetic"
+                />
+              </div>
+              <div
+                className={classNames(
+                  styles.proposals_grid__row__element,
+                  styles.last,
+                  "mb-3",
+                )}
+              >
+                {NumbersService.parseNumericValue(formatUnits(votes.total, 18))}
+              </div>
+            </div>
+          ))}
         </div>
       </Card>
     </div>

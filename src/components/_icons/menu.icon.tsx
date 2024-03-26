1873d8c2130c62233502a834b2f7b6c0bdbcfe62
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-03-26 17:21:57 -0500

    fix: revert change made in conflict resolution

--- src/components/_icons/menu.icon.tsx
+++ src/components/_icons/menu.icon.tsx
@@ -1,4 +1,4 @@
-import BaseIconProps from "@interfaces/base-icon-props.interface";
+import BaseIconProps from "@/interfaces/base-icon-props.interface";
 import classNames from "classnames";
 
 interface MenuIconProps extends BaseIconProps {

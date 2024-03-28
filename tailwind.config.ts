f7611789dc53a2896fadbdb455e3f879468d3f6a
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-03-28 18:22:04 -0500

    fix: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "/src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -41,9 +43,6 @@ const config: Config = {
         "0%": { transform: "rotate(0deg)" },
         "100%": { transform: "rotate(360deg)" },
       },
-    },
-    animation: {
-      "spin-slow": "spin-slow 1.5s linear infinite",
     },
   },
   plugins: [require("@tailwindcss/typography")],

b879c8c58cce8d9ee2d8405fe52ada16f0006a6b
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-03-28 18:22:21 -0500

    fix: de-sass divider

--- src/components/_shared/divider/divider.component.tsx
+++ src/components/_shared/divider/divider.component.tsx
@@ -3,11 +3,6 @@ import BaseComponentProps from "@/interfaces/base-component-props.interface";
 
 export const Divider = ({ className }: BaseComponentProps) => {
   return (
-    <hr
-      className={classNames(
-        "h-px border-0 bg-light-gray mt-4 mb-5 md:mt-5",
-        className,
-      )}
-    />
+    <div className={classNames("border-b border-light-gray", className)} />
   );
 };

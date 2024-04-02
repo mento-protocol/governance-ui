2e95ff106cb2e999d0db0daa92404817af7d0371
Ryan Noble <ryanjnoble@gmail.com>
2024-04-02 18:38:02 +0200

    Theme switch & vote list

--- src/components/theme-switch/theme-switch.component.tsx
+++ src/components/theme-switch/theme-switch.component.tsx
@@ -2,17 +2,15 @@ import BaseComponentProps from "@/interfaces/base-component-props.interface";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { DarkModeIcon, LightModeIcon } from "@/components/_icons";
-import styles from "./theme-switch.module.scss";
 import { LocalStorageKeys, useLocalStorage } from "@/lib/hooks/useStorage";
 
-enum Mode {
+const enum Mode {
   LIGHT = "light",
   DARK = "dark",
 }
 
 export const ThemeSwitch = ({ className, style }: BaseComponentProps) => {
-  const [selectedMode, setSelectedMode] = useState<string | null>(null);
-  const [defaultMode, setDefaultMode] = useState(Mode.LIGHT);
+  const [selectedMode, setSelectedMode] = useState<Mode | null>(null);
 
   const { getItem, setItem } = useLocalStorage(
     LocalStorageKeys.DARK_MODE_TOGGLE,
@@ -21,15 +19,20 @@ export const ThemeSwitch = ({ className, style }: BaseComponentProps) => {
   useEffect(() => {
     const initialTheme = window.matchMedia("(prefers-color-scheme: dark)");
     const theme = getItem("theme");
-    if (theme) setSelectedMode(theme);
-    if (initialTheme) setDefaultMode(initialTheme ? Mode.DARK : Mode.LIGHT);
+
+    if (theme) {
+      setSelectedMode(theme as Mode);
+    } else {
+      setSelectedMode(initialTheme ? Mode.DARK : Mode.LIGHT);
+    }
   }, [getItem]);
 
   useEffect(() => {
-    if (selectedMode) setItem("theme", selectedMode);
-    if (selectedMode || defaultMode)
-      document.body.className = selectedMode || defaultMode;
-  }, [selectedMode, defaultMode, setItem]);
+    if (selectedMode) {
+      setItem("theme", selectedMode);
+      document.body.className = selectedMode;
+    }
+  }, [selectedMode, setItem]);
 
   const onToggleSwitch = () => {
     setSelectedMode(selectedMode === Mode.DARK ? Mode.LIGHT : Mode.DARK);
@@ -40,16 +43,17 @@ export const ThemeSwitch = ({ className, style }: BaseComponentProps) => {
       onClick={onToggleSwitch}
       className={classNames(
         "relative flex h-[24px] w-[44px] items-center justify-between rounded-[32px] px-x1 py-[3px]",
-        styles[`switch__${selectedMode || defaultMode}`],
-        className,
+        selectedMode === Mode.LIGHT ? "bg-white" : "bg-secondary",
+        selectedMode === className,
       )}
       style={style}
     >
-      <div className={styles.switch__mode}>
+      <div className="flex w-full items-center justify-between gap-x1">
         <div
           className={classNames(
             "ease absolute h-[18px] w-[18px] rounded-[9px] bg-black transition-all duration-[400ms]",
-            styles[`circle__${selectedMode || defaultMode}`],
+            // TODO: refactor to use dynamic percentage widths
+            selectedMode === Mode.LIGHT ? "left-[3px]" : "left-[21px]",
           )}
         />
         <div className="flex w-full justify-between">

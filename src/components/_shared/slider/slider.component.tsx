17530c95bd184b9e1c8a350498705a8eb7e47eac
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-04-16 23:33:11 -0500

    refactor: simplify slider

--- src/components/_shared/slider/slider.component.tsx
+++ src/components/_shared/slider/slider.component.tsx
@@ -1,90 +1,57 @@
 "use client";
-import { ReactNode, useEffect, useMemo, useState } from "react";
-import BaseComponentProps from "@/interfaces/base-component-props.interface";
-import BaseInputProps from "@/interfaces/base-input-props.interface";
-import exports from "@/styles/exports.module.scss";
-import styles from "./slider.module.scss";
+
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
 import { cn } from "@/styles/helpers";
 
-interface SliderProps extends BaseComponentProps, BaseInputProps {
-  compact?: boolean;
-  min?: number;
-  max?: number;
-  step?: number;
-  minLabel?: ReactNode | string;
-  maxLabel?: ReactNode | string;
-  bubbleFormatter?: (value: number) => ReactNode | string;
-  changeCallback?: (date: number | string) => void;
-}
-
-export const Slider = ({
-  label,
-  id,
-  className,
-  form,
-  error,
-  compact,
-  min,
-  max,
-  step,
-  minLabel,
-  maxLabel,
-  bubbleFormatter,
-  value,
-  changeCallback,
-}: SliderProps) => {
-  const { onBlur, onChange, ref, name } = form;
-  const [currentValue, setCurrentValue] = useState(min || value);
-
-  useEffect(() => {
-    setCurrentValue(value);
-  }, [value]);
-
-  const activeBackground = useMemo(() => {
-    const progress = Math.floor((currentValue / (max || 100)) * 100);
-    return `linear-gradient(90deg, ${exports.secondary} ${progress}%, transparent ${progress}%)`;
-  }, [currentValue, max]);
-
-  const bubblePosition = useMemo(() => {
-    const progress = Math.floor((currentValue / (max || 100)) * 100);
-    return `calc(${progress - (100 - progress) / 100}% - ${(progress * 44) / 100}px)`;
-  }, [currentValue, max]);
-
-  const onValueChange = (event: any) => {
-    setCurrentValue(event.target.value);
-    changeCallback && changeCallback(event.target.value);
-    onChange(event);
-  };
-
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> & {
+    labels?: {
+      min?: string;
+      max?: string;
+      current?: React.ReactNode;
+    };
+  }
+>(({ className, labels, ...props }, ref) => {
   return (
-    <div
-      className={cn(styles.sliderWrapper, compact && styles.compact, className)}
-    >
-      {!!label && <label htmlFor={id}>{label}</label>}
-      <div className={styles.bubbleTrack}>
-        <div className={styles.bubble} style={{ left: bubblePosition }}>
-          {bubbleFormatter ? bubbleFormatter(currentValue) : currentValue}
-        </div>
+    <div className=" mt-10 flex flex-col gap-2">
+      <div className="flex justify-between text-[#636366]">
+        <label className="-translate-x-5" key={labels?.min}>
+          {labels?.min}
+        </label>
+        <label className="translate-x-5" key={labels?.max}>
+          {labels?.max}
+        </label>
       </div>
-      <div className={styles.labels}>
-        <div>{minLabel}</div>
-        <div>{maxLabel}</div>
-      </div>
-      <input
-        id={id}
-        step={step}
-        className={cn(styles.slider)}
-        onChange={onValueChange}
-        onBlur={onBlur}
+      <SliderPrimitive.Root
         ref={ref}
-        name={name}
-        value={currentValue}
-        style={{ background: activeBackground }}
-        min={min}
-        max={max}
-        type={"range"}
-      />
-      {!!error && <div className={styles.errorMessage}>{error}</div>}
+        className={cn(
+          "relative flex w-full touch-none select-none items-center",
+          className,
+        )}
+        {...props}
+      >
+        <SliderPrimitive.Track className="relative h-[34px] w-full grow overflow-hidden rounded-lg border border-black">
+          <SliderPrimitive.Range className="absolute h-full bg-mento-blush" />
+        </SliderPrimitive.Track>
+        <SliderPrimitive.Thumb className=" focus-visible:ring-ring relative block h-11 w-11 rounded-lg border border-black bg-black shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50">
+          {props.min &&
+          props.max &&
+          props.value &&
+          props?.value?.[0] > props?.min &&
+          props?.value?.[0] < props?.max ? (
+            <span className="absolute -top-10 flex text-[#636366]">
+              <div className="flex w-fit whitespace-nowrap">
+                {labels?.current}
+              </div>
+            </span>
+          ) : null}
+        </SliderPrimitive.Thumb>
+      </SliderPrimitive.Root>
     </div>
   );
-};
+});
+Slider.displayName = SliderPrimitive.Root.displayName;
+
+export { Slider };

767f1eaa211e1f5a521242c8d22ee162b5b1f799
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-04-19 05:48:16 -0500

    fix: UI error on voting page when voting against

--- src/components/vote-cast-vote/vote-confirmation.tsx
+++ src/components/vote-cast-vote/vote-confirmation.tsx
@@ -16,27 +16,21 @@ export const VoteConfirmation = ({
       <div className="mt-x2 flex flex-col gap-x3 text-center">
         <span className="text-md">Confirm your vote</span>
         <Loader isCenter />
-        {voteType && (
-          <>
-            <div className="flex flex-col items-center justify-center gap-1 text-[0.875rem]">
-              <div className="flex items-center justify-center gap-1">
-                <span>{`Voting `}</span>
-                <span className="flex items-center justify-center">
-                  <VoteTypePill voteType={voteType} />
-                </span>
-                <span>on</span>
-              </div>
-              <div>
-                <span>Proposal </span>
-                <span className="font-semibold">{`${WalletHelper.getShortAddress(proposalId)}`}</span>
-              </div>
+        <>
+          <div className="flex flex-col items-center justify-center gap-1 text-[0.875rem]">
+            <div className="flex items-center justify-center gap-1">
+              <span>{`Voting `}</span>
+              <span className="flex items-center justify-center">
+                <VoteTypePill voteType={voteType} />
+              </span>
+              <span>on</span>
             </div>
             <div>
               <span>Proposal </span>
               <span className="font-semibold">{`${WalletHelper.getShortAddress(proposalId)}`}</span>
             </div>
-          </>
-        )}
+          </div>
+        </>
         <span className="text-sm text-[#A8A8A8] dark:text-[#AAB3B6]">
           Proceed in your wallet
         </span>

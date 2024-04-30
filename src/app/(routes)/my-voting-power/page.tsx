362889e82b4f014267922bdf9025a91d173c4e0a
denviljclarke <60730266+denviljclarke@users.noreply.github.com>
2024-04-30 09:13:40 -0500

    feat: add  connect wallet message to voting page

--- src/app/(routes)/my-voting-power/page.tsx
+++ src/app/(routes)/my-voting-power/page.tsx
@@ -1,24 +1,31 @@
 "use client";
 import { useAccount } from "wagmi";
-import { Card, Loader, MentoLock } from "@/components/_shared";
+import { Card, ConnectButton, Loader, MentoLock } from "@/components/_shared";
 import { LocksList } from "@/components/index";
 import useLocksByAccount from "@/lib/contracts/locking/useLocksByAccount";
 
 const Page = () => {
-  const { address, isConnecting, isDisconnected } = useAccount();
+  const { address, isConnecting } = useAccount();
   const { locks, refetch } = useLocksByAccount({ account: address! });
   return (
     <main className="flex flex-col place-items-center">
       <h2 className="mb-4 mt-8 text-2xl">My Voting Power</h2>
-      <Card className="flex items-center justify-center py-10" block>
-        <MentoLock
-          onLockConfirmation={() => {
-            refetch();
-          }}
-          className="max-w-[428px]"
-        />
+      <Card className="flex min-h-60 items-center justify-center py-10" block>
+        {address ? (
+          <MentoLock
+            onLockConfirmation={() => {
+              refetch();
+            }}
+            className="max-w-[428px]"
+          />
+        ) : (
+          <div className="flex flex-col gap-4 ">
+            <span className="text-xl">Connect Wallet to Lock MENTO</span>
+            <ConnectButton theme="primary" />
+          </div>
+        )}
       </Card>
-      {!isDisconnected && (
+      {address && (
         <>
           <h2 className="mb-4 mt-8 text-2xl">My Locks</h2>
           <Card block>

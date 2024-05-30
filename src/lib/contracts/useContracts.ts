7f21fed973df4616bf2ad2d2543bc835d5377448
Ryan Noble <ryanjnoble@gmail.com>
2024-05-30 15:09:43 +0200

    Fix: Force correct network

--- src/lib/contracts/useContracts.ts
+++ src/lib/contracts/useContracts.ts
@@ -1,10 +1,12 @@
 import { useAccount, useChains } from "wagmi";
 import { MentoChain, MentoChainContracts } from "@/lib/types";
 import { Celo, Alfajores } from "@/config/chains";
+import { useEnsureChain } from "@/lib/hooks/useEnsureChain";
 
 export const useContracts = (): MentoChainContracts => {
   const chains = useChains();
   const { isConnected, chainId } = useAccount();
+  useEnsureChain();
 
   return isConnected && (chainId === Celo.id || chainId === Alfajores.id)
     ? (chains.find((chain) => chain.id === chainId) as MentoChain).contracts

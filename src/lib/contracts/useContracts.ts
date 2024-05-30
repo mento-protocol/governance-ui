fac83c2315bef40d8b227a93b40821b29c982faf
Ryan Noble <ryanjnoble@gmail.com>
2024-05-30 14:42:43 +0200

    Fix: Crashing on other networks

--- src/lib/contracts/useContracts.ts
+++ src/lib/contracts/useContracts.ts
@@ -1,12 +1,12 @@
 import { useAccount, useChains } from "wagmi";
 import { MentoChain, MentoChainContracts } from "@/lib/types";
-import { Celo } from "@/config/chains";
+import { Celo, Alfajores } from "@/config/chains";
 
 export const useContracts = (): MentoChainContracts => {
   const chains = useChains();
   const { isConnected, chainId } = useAccount();
 
-  return isConnected
+  return isConnected && (chainId === Celo.id || chainId === Alfajores.id)
     ? (chains.find((chain) => chain.id === chainId) as MentoChain).contracts
     : Celo.contracts;
 };

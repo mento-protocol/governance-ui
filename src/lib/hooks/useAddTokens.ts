06317253e581cb229e9110d43153cb1e69481715
Ryan Noble <ryanjnoble@gmail.com>
2024-06-14 13:37:09 +0200

    Fix: Using viem watchAsset to add

--- src/lib/hooks/useAddTokens.ts
+++ src/lib/hooks/useAddTokens.ts
@@ -1,52 +1,48 @@
 "use client";
 import { useCallback } from "react";
 import * as mento from "@mento-protocol/mento-sdk";
-import { useAccount, useClient } from "wagmi";
+import { useAccount, useClient, useConfig } from "wagmi";
 import { Alfajores, Celo } from "@/config/chains";
+import { getConnectorClient } from "wagmi/actions";
+import { watchAsset } from "viem/actions";
 
 export const useAddTokens = () => {
   const { chainId } = useAccount();
   const client = useClient();
+  const config = useConfig();
 
   const addMento = useCallback(async () => {
-    if (!chainId || !client?.request) return;
-    await client.request({
-      method: "wallet_watchAsset",
-      params: {
-        type: "ERC20", // Initially only supports ERC20, but eventually more!
-        options: {
-          address:
-            mento.addresses[
-              chainId !== Celo.id || chainId !== Alfajores.id
-                ? Celo.id
-                : chainId
-            ].MentoToken, // The address that the token is at.
-          symbol: "MENTO", // A ticker symbol or shorthand, up to 5 chars.
-          decimals: 18, // The number of decimals in the token
-        },
+    const connectorClient = await getConnectorClient(config);
+
+    return await watchAsset(connectorClient, {
+      type: "ERC20",
+      options: {
+        address:
+          mento.addresses[
+            chainId !== Celo.id || chainId !== Alfajores.id ? Celo.id : chainId
+          ].MentoToken,
+        symbol: "MENTO",
+        decimals: 18,
       },
     });
-  }, [chainId, client]);
+  }, [chainId, config]);
 
   const addVeMento = useCallback(async () => {
     if (!chainId || !client?.request) return;
-    await client.request({
-      method: "wallet_watchAsset",
-      params: {
-        type: "ERC20", // Initially only supports ERC20, but eventually more!
-        options: {
-          address:
-            mento.addresses[
-              chainId !== Celo.id || chainId !== Alfajores.id
-                ? Celo.id
-                : chainId
-            ].Locking, // The address that the token is at.
-          symbol: "veMENTO", // A ticker symbol or shorthand, up to 5 chars.
-          decimals: 18, // The number of decimals in the token
-        },
+    const connectorClient = await getConnectorClient(config);
+
+    return await watchAsset(connectorClient, {
+      type: "ERC20",
+      options: {
+        address:
+          mento.addresses[
+            chainId !== Celo.id || chainId !== Alfajores.id ? Celo.id : chainId
+          ].Locking,
+        symbol: "veMENTO",
+        decimals: 18,
       },
     });
-  }, [chainId, client]);
+  }, [chainId, client?.request, config]);
 
   return {
     addMento,

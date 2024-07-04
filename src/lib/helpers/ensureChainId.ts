8fde8e2d219f0b76bc6ec965acdfd9d5d90fe78b
Ryan Noble <ryanjnoble@gmail.com>
2024-07-04 08:50:01 +0200

    Hotfix: Alfajores subgraph patch (#203)

--- src/lib/helpers/ensureChainId.ts
+++ src/lib/helpers/ensureChainId.ts
@@ -1,7 +1,7 @@
 import { Alfajores, Celo } from "@/config/chains";
 
 export const ensureChainId = (chainId?: number) => {
-  if (chainId !== Celo.id || chainId !== Alfajores.id) {
+  if (chainId !== Celo.id && chainId !== Alfajores.id) {
     return Celo.id;
   } else {
     return chainId;

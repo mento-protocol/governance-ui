9ed613e304d1bb599c071b026a579730aa881b87
Bayological <6872903+bayological@users.noreply.github.com>
2024-07-08 09:24:01 -0500

    refactor: use empty string for root path

--- src/lib/helpers/routing.map.ts
+++ src/lib/helpers/routing.map.ts
@@ -1,5 +1,5 @@
 export const routingMap: Map<string, string> = new Map([
-  ["", "Proposals"],
+  ["", ""],
   ["my-voting-power", "My Voting Power"],
   ["create-proposal", "Create a proposal"],
   ["proposals", "Proposal"],

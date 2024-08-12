9dd3fc9a17af633e4ec38836be3a650a0da30bd5
Denvil J Clarke <60730266+denviljclarke@users.noreply.github.com>
2024-08-12 18:42:19 +0100

    Feat: Extend Lock (#215)

---
+++ src/app/voting-power/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useAccount } from "wagmi";
+import { Card, ConnectButton, MentoLock } from "@/components/_shared";
+import { LocksList } from "@/components/index";
+import useLocksByAccount from "@/lib/contracts/locking/useLocksByAccount";
+
+const Page = () => {
+  const { address } = useAccount();
+  const { refetch } = useLocksByAccount({ account: address! });
+
+  return (
+    <main className="flex flex-col place-items-center gap-14">
+      <div className="flex w-full flex-col gap-4 text-center md:gap-6">
+        <h2 className="mb-4 mt-[56px] text-[22px]/none font-medium md:text-[32px]/none">
+          Your existing veMENTO lock
+        </h2>
+        <LocksList />
+      </div>
+
+      <div className="flex w-full flex-col items-center gap-8">
+        <h2 className="text-[22px]/none font-medium md:text-[32px]/none">
+          Lock MENTO
+        </h2>
+        <Card className="flex min-h-60 items-center justify-center py-10" block>
+          {address ? (
+            <MentoLock
+              onLockConfirmation={() => {
+                refetch();
+              }}
+              className="max-w-[428px]"
+            />
+          ) : (
+            <Disconnected />
+          )}
+        </Card>
+      </div>
+    </main>
+  );
+};
+
+const Disconnected = () => {
+  return (
+    <div className="flex flex-col gap-4 ">
+      <span className="text-xl">Connect Wallet to Lock MENTO</span>
+      <ConnectButton theme="primary" />
+    </div>
+  );
+};
+
+export default Page;
--- src/lib/helpers/routing.map.ts
+++ src/lib/helpers/routing.map.ts
@@ -1,6 +1,6 @@
 export const routingMap: Map<string, string> = new Map([
   ["", ""],
-  ["my-voting-power", "My Voting Power"],
+  ["voting-power", "Voting Power"],
   ["create-proposal", "Create a proposal"],
   ["proposals", "Proposal"],
 ]);

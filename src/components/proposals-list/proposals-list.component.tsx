91b511736c2b444193c514e8d2fa90839474e7e1
philbow61 <80156619+philbow61@users.noreply.github.com>
2024-09-10 10:19:59 +0200

    fix: mobile proposal list (#227)

--- src/components/proposals-list/proposals-list.component.tsx
+++ src/components/proposals-list/proposals-list.component.tsx
@@ -2,37 +2,24 @@
 import NumbersService from "@/lib/helpers/numbers.service";
 import StringService from "@/lib/helpers/string.service";
 import BaseComponentProps from "@/lib/interfaces/base-component-props.interface";
-import { Card, ProgressBar, Status } from "@/components/_shared";
+import { ProgressBar, Status } from "@/components/_shared";
 import Link from "next/link";
 import { stateToStatusColorMap } from "@/lib/interfaces/proposal.interface";
 import { formatUnits } from "viem";
 import useProposals from "@/lib/contracts/governor/useProposals";
-import { cn } from "@/styles/helpers";
+import { Proposal } from "@/lib/graphql/subgraph/generated/subgraph";
 import { EmptyProposals } from "../_icons";
+import { MentoIcon } from "../_icons";
 
 interface ProposalsListProps extends BaseComponentProps {}
 
 export const ProposalsListComponent = ({ className }: ProposalsListProps) => {
   return (
     <div className={`w-full font-fg ${className}`}>
-      <h2 className="mb-x6 mt-x11 text-center text-[32px] font-medium">
+      <h2 className="pb-[32px] pt-[30px] text-center text-[22px] font-medium md:pb-[34px] md:pt-[76px]">
         Proposals
       </h2>
-      <Card block>
-        <div
-          className={cn(
-            "overflow-x-hidden",
-            "bg-transparent bg-[20px_100%,20px_100%,10px_100%,10px_100%] bg-no-repeat  [background-attachment:local,local,scroll,scroll] [background-position:left_center,right_center,left_center,right]",
-            "text_small",
-            `[background-image:linear-gradient(to_right,#FFFFFF,#FFFFFF),linear-gradient(to_right,#FFFFFF,#FFFFFF),linear-gradient(to_right,rgba(0,_0,_0,_0.15),transparent),linear-gradient(to_left,rgba(0,_0,_0,_0.15),transparent)]`,
-            `dark:[background-image:linear-gradient(to_right,#121316,#121316),linear-gradient(to_right,#121316,#121316),linear-gradient(to_right,rgba(156,_156,_156,_0.25),transparent),linear-gradient(to_left,rgba(156,_156,_156,_0.25),transparent)]`,
-          )}
-        >
-          {/* Line height 24 OG: 25, letter spacing -0.304px, now 0.05em */}
-
-          <ProposalsTable />
-        </div>
-      </Card>
+      <ProposalsTable />
     </div>
   );
 };
@@ -47,58 +34,51 @@ const ProposalsTable = () => {
       </div>
     );
   }
-
   return (
     <>
-      <div className="mb-[38px] hidden grid-cols-[350px_100px_150px_150px_150px] items-center gap-[21px] font-inter font-medium md:visible md:grid md:grid-cols-[minmax(150px,_2fr)_100px_150px_150px_150px]">
-        <div
-          className={
-            "overflow-hidden border-none text-left font-inter text-base font-medium tracking-tighter"
-          }
-        >
+      <div className="hidden lg:block">
+        <DesktopProposalTable proposals={proposals} />
+      </div>
+      <div className="lg:hidden">
+        <MobileProposalTable proposals={proposals} />
+      </div>
+    </>
+  );
+};
+
+const DesktopProposalTable = ({ proposals }: { proposals: Proposal[] }) => {
+  return (
+    <div className="rounded-md border border-gray-light bg-white px-4 py-5 dark:bg-black-off">
+      <div className="mb-[38px] grid grid-cols-[minmax(150px,_2fr)_100px_150px_150px_150px] items-center gap-[21px] font-inter font-medium">
+        <div className="overflow-hidden border-none text-left font-inter text-base font-medium tracking-tighter">
           Proposal name
         </div>
-        <div
-          className={
-            "overflow-hidden border-none text-center font-inter text-base font-medium tracking-tighter"
-          }
-        >
+        <div className="overflow-hidden border-none text-center font-inter text-base font-medium tracking-tighter">
           Status
         </div>
-        <div
-          className={
-            "overflow-hidden border-none text-center font-inter text-base font-medium tracking-tighter"
-          }
-        >
+        <div className="overflow-hidden border-none text-center font-inter text-base font-medium tracking-tighter">
           Votes in favor
         </div>
-        <div
-          className={
-            "overflow-hidden border-none text-center font-inter text-base font-medium tracking-tighter"
-          }
-        >
+        <div className="overflow-hidden border-none text-center font-inter text-base font-medium tracking-tighter">
           Votes against
         </div>
-        <div
-          className={
-            "overflow-hidden border-none text-right font-inter text-base font-medium tracking-tighter"
-          }
-        >
+        <div className="overflow-hidden border-none text-right font-inter text-base font-medium tracking-tighter">
           Total votes
         </div>
       </div>
       {proposals.map(({ proposalId, metadata, state, votes }, index) => (
         <div
           key={index}
-          className="mb-[21px] grid grid-cols-[60%_40%] items-start gap-2 font-medium last:mb-0 sm:grid-cols-[350px_100px_150px_150px_150px] md:grid-cols-[minmax(150px,_2fr)_100px_150px_150px_150px] md:gap-[21px]"
+          className="grid grid-cols-[minmax(150px,_2fr)_100px_150px_150px_150px] items-start gap-[18px] pb-[18px] font-medium"
         >
           {!!index && (
             <div className="border-b border-solid border-gray-light [grid-column:1_/_-1]" />
           )}
-          <div className="order-1 self-center overflow-hidden text-ellipsis break-words text-left text-lg font-normal sm:max-w-[340px]">
+          <div className="self-center overflow-hidden text-ellipsis break-words text-left text-lg font-normal">
             <div className="flex place-items-center gap-x3">
+              <MentoIcon className="h-[42px] w-[40px]" backgroundColor="cyan" />
               <Link
-                className="flex "
+                className="flex"
                 style={{ maxHeight: "3em" }}
                 href={`/proposals/${proposalId}`}
               >
@@ -109,20 +89,20 @@ const ProposalsTable = () => {
             </div>
           </div>
           <Status
-            className="order-3 h-auto self-center sm:order-2 sm:h-[22px] sm:w-full"
+            className="h-[22px] w-full self-center"
             text={state?.toString()}
             type={stateToStatusColorMap[state]}
           />
-          <div className="order-2 flex items-center self-center overflow-hidden text-center md:order-3">
+          <div className="flex items-center self-center overflow-hidden text-center">
             <ProgressBar
               type="success"
-              className="order-2 mx-auto my-0 w-full max-w-[110px] gap-x1 sm:order-3"
+              className="mx-auto my-0 w-full max-w-[110px] gap-x1"
               current={Number(formatUnits(votes.for.total, 18))}
               max={Number(formatUnits(votes.total, 18))}
               valueFormat="alphabetic"
             />
           </div>
-          <div className="order-4 flex items-center self-center overflow-hidden text-center">
+          <div className="flex items-center self-center overflow-hidden text-center">
             <ProgressBar
               type="danger"
               className="mx-auto my-0 w-full max-w-[110px] gap-x1"
@@ -131,11 +111,66 @@ const ProposalsTable = () => {
               valueFormat="alphabetic"
             />
           </div>
-          <div className="order-5 mr-x1 hidden self-center overflow-hidden text-right text-[22px]/[23px] font-normal md:visible md:block">
+          <div className="mr-x1 self-center overflow-hidden text-right text-[22px]/[23px] font-normal">
             {NumbersService.parseNumericValue(formatUnits(votes.total, 18))}
           </div>
         </div>
       ))}
-    </>
+      <div className="left-0 right-0 flex border-b border-solid border-gray-light " />
+    </div>
+  );
+};
+
+const MobileProposalTable = ({ proposals }: { proposals: Proposal[] }) => {
+  return (
+    <div className="rounded-md border border-gray-light bg-white px-4 py-5 dark:bg-black-off">
+      {proposals.map(({ proposalId, metadata, state, votes }, index) => (
+        <div
+          key={index}
+          className="relative grid grid-cols-[60%_40%] items-start pb-[8px] pt-[20px] font-medium first:pt-0 last:mb-0"
+        >
+          {!!index && (
+            <div className="absolute left-0 right-0 border-b border-solid border-gray-light " />
+          )}
+
+          <div className="font-size-[18px] flex self-start overflow-hidden text-ellipsis break-words text-left font-normal leading-5">
+            <Link
+              className="flex"
+              style={{ maxHeight: "3em" }}
+              href={`/proposals/${proposalId}`}
+            >
+              <p>{StringService.limitLength(`${metadata?.title}`, 75, true)}</p>
+            </Link>
+          </div>
+          <div className="flex justify-end overflow-hidden pb-[8px]">
+            <ProgressBar
+              type="success"
+              className="align-right w-full max-w-[110px] "
+              current={Number(formatUnits(votes.for.total, 18))}
+              max={Number(formatUnits(votes.total, 18))}
+              valueFormat="alphabetic"
+            />
+          </div>
+          <div className="flex overflow-hidden pt-[8px]">
+            <Status
+              className="h-auto self-center"
+              text={state?.toString()}
+              type={stateToStatusColorMap[state]}
+            />
+          </div>
+
+          <div className="flex justify-end overflow-hidden pb-[8.35px] ">
+            <ProgressBar
+              type="danger"
+              className="align-right w-full max-w-[110px]"
+              current={Number(formatUnits(votes.against.total, 18))}
+              max={Number(formatUnits(votes.total, 18))}
+              valueFormat="alphabetic"
+            />
+          </div>
+        </div>
+      ))}
+      <div className="left-0 right-0 flex border-b border-solid border-gray-light " />
+    </div>
   );
 };
